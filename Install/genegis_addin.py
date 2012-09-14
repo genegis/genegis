c943ad9eca372efc2781969fc453b77dec7d39ed
Shaun Walbridge <shaun.walbridge@gmail.com>
2012-09-13 18:53:01 -0700

    initial pass at filtering the intersecting results on the fly, present a simple summary of results.

--- Install/genegis_addin.py
+++ Install/genegis_addin.py
@@ -59,32 +59,49 @@ class ToolClass2(object):
 
     def onRectangle(self, rectangle_geometry):
         extent = rectangle_geometry
-        # the selected layer
+        # the current _selected_ layer in ArcMap
         layer = _currentLayer()
         if layer is None:
-            return None    
+            return None
 
        # extract the coordinates from our extent object
         coords = [[extent.XMin,extent.YMin],[extent.XMax,extent.YMin], \
                 [extent.XMax,extent.YMax],[extent.XMin,extent.YMax]]
-        # convert it to a polygon, we need this to compute the intersection
 
+        # convert it to a polygon, we need this to compute the intersection
         polygon_extent = arcpy.Polygon(arcpy.Array(
             [arcpy.Point(x,y) for x,y in coords]), arcpy.SpatialReference(4326))
 
-        print type(polygon_extent)
-        # create an extent object from our selection
-        #output_feature = 'in_memory\selection_a_poly'
+        # perform an intersection. Can take an optional 'add to selection' vs. 'new selection'
+        selection_results = arcpy.SelectLayerByLocation_management(
+                layer.name, "INTERSECT", polygon_extent)
 
-        # XXX get the selected layer's spatial reference:
-        # arcpy.SpatialReference(...) OR point at a prj file.
+        # store the results in memory, no need to bring spindles into this
+        output_feature = 'in_memory/selection_a_poly'
+        if arcpy.Exists(output_feature):
+            arcpy.Delete_management(output_feature)
+        arcpy.CopyFeatures_management(selection_results.getOutput(0), output_feature)
 
-        #if arcpy.Exists(output_feature):
-            
-        # now, grab the current _selected_ layer in ArcMap
+        id_field = "Individual_ID"
+        fields = [f.name for f in arcpy.ListFields(output_feature)]
+        if id_field in fields:
+            cur = arcpy.da.SearchCursor(output_feature, (id_field))
+            individuals = [row[0] for row in cur]
+            unique_individuals = set(individuals)
+            msg = "Samples: {0}, Unique Individuals: {1}".format(len(individuals), len(unique_individuals))
+            title = "whales, ohh yeah!"
+            pythonaddins.MessageBox(msg, title)
+        else:
+            print "Couldn't find an individual ID field!"
+ 
+        """
+        so we'd probably want to identify the specific columns of interest (haplotypes?),
+        perhaps using a drop-down menu, and then use the select tool to generate our areas of
+        interest, and shoot back out some summary statistics based on those observations.
+        """
 
-        # XXX here either use 'Spatial Join' or 'Select Features by Location'
-        #arcpy.SpatialJoin_analysis(
+        # just a string representation of the feature name
+        return output_feature
 
 class ToolClass4(object):
     """Implementation for genegis_addin.tool_1 (Tool)"""
