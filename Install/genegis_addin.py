3f6d0398d0f1a041afa5ed53c66d161810a60ced
Shaun Walbridge <shaun.walbridge@gmail.com>
2012-09-27 21:42:07 -0700

    remove unneeded template methods

--- Install/genegis_addin.py
+++ Install/genegis_addin.py
@@ -145,26 +145,6 @@ class CompareEncounters(object):
     def __init__(self):
         self.enabled = True
         self.shape = "NONE" # Can set to "Line", "Circle" or "Rectangle" for interactive shape drawing and to activate the onLine/Polygon/Circle event sinks.
-    def onMouseDown(self, x, y, button, shift):
-        pass
-    def onMouseDownMap(self, x, y, button, shift):
-        pass
-    def onMouseUp(self, x, y, button, shift):
-        pass
-    def onMouseUpMap(self, x, y, button, shift):
-        pass
-    def onMouseMove(self, x, y, button, shift):
-        pass
-    def onMouseMoveMap(self, x, y, button, shift):
-        pass
-    def onDblClick(self):
-        pass
-    def onKeyDown(self, keycode, shift):
-        pass
-    def onKeyUp(self, keycode, shift):
-        pass
-    def deactivate(self):
-        pass
     def onRectangle(self, rectangle_geometry):
         pass
 
