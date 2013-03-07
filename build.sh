28ef8a0e734f728259e95e94516e79aeb7ebe76b
Shaun Walbridge <shaun.walbridge@gmail.com>
2013-03-07 03:00:09 -0800

    Include in-depth build process, as used in BTM. fixes #35.

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/bash
+echo "building Addin..."
 python ./makeaddin.py
+echo "signing Addin..."
+/c/apps/arcgis/Desktop10.1/bin/ESRISignAddin.exe c:\\data\\arcgis\\addins\\genegis\\genegis.esriaddin /c:c:\\data\\arcgis\\addins\\cert.cer
 start genegis.esriaddin
-start C:\Users\shau7031\Documents\ArcGIS\genegis.mxd
+start /c/Users/shau7031/Documents/ArcGIS/genegis.mxd
