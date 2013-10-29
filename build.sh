772bee7baa305e28d251a18fca5f46ecdeb27e5d
Shaun Walbridge <shaun.walbridge@gmail.com>
2013-10-29 03:11:44 -0400

    Update build script for 10.2; fixes #85

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 echo "building Addin..."
 python ./makeaddin.py
 echo "signing Addin..."
-/c/apps/arcgis/Desktop10.1/bin/ESRISignAddin.exe c:\\data\\arcgis\\addins\\genegis\\genegis.esriaddin /c:c:\\data\\arcgis\\addins\\cert.cer
+"/c/Program Files (x86)/Common Files/ArcGIS/bin/ESRISignAddin.exe" c:\\data\\arcgis\\addins\\genegis\\genegis.esriaddin /c:c:\\data\\arcgis\\addins\\cert.cer
 start genegis.esriaddin
 start /c/Users/shau7031/Documents/ArcGIS/genegis.mxd
