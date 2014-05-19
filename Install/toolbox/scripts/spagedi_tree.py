fd7a147d20b869deadacc89efec525071c5d2a43
Jack Peterson <jack@tinybike.net>
2014-05-18 23:21:11 -0700

    decision tree sublayer fixes

--- Install/toolbox/scripts/spagedi_tree.py
+++ Install/toolbox/scripts/spagedi_tree.py
@@ -4,29 +4,27 @@ from bunch import Bunch, bunchify
 
 class TreeMaker(object):
 
-    def __init__(self, output_options):
-        self.output_options = output_options
-
     def fasten(self, tree, next, ignore_keys=('headline', 'default')):
         if type(tree) == Bunch:
             # If there is already a link to the next level, descend
-            if 'next' in tree:
-                tree = self.fasten(tree.next, next)
-            else:
-                # If free-form user input is expected, 'next' is on the same level
-                if 'user_input' in tree:
-                    tree.next = next
-                # If a menu selection is expected, 'next' is one level down
-                else:
-                    for node in tree:
-                        if type(tree[node]) == Bunch and node not in ignore_keys:
-                            if 'next' in tree[node]:
-                                tree = self.fasten(tree[node], next)
-                            else:
-                                tree[node].next = next
-                        # If the value is null, skip to output options
-                        elif tree[node] is None:
-                            tree[node] = self.output_options
+            # if 'next' in tree:
+            #     tree = self.fasten(tree.next, next)
+            # else:
+            for node in tree:
+                # if type(tree[node]) == Bunch and node not in ignore_keys:
+                if node not in ignore_keys:
+                    # if 'next' in tree[node]:
+                    #     tree = self.fasten(tree[node], next)
+                    # elif type(tree[node][tree[node].keys()[0]]) == Bunch:
+                    #     for subnode in tree[node]:
+                    #         if tree[node][subnode] and subnode not in ignore_keys:
+                    #             tree[node][subnode].next = next
+                    #         elif tree[node][subnode] is None:
+                    #             tree[node][subnode].next = self.output_options
+                    # else:
+                    tree[node].next = next
+                # elif tree[node] is None:
+                #     tree[node].next = self.output_options
         return tree
 
     def buildtree(self, tree, ordering):
@@ -35,11 +33,13 @@ class TreeMaker(object):
         for lyr in ordering[1:-1]:
             subtree = tree.pop(below, None)
             if 'individual' in subtree:
-                tree[lyr].individual = self.fasten(tree[lyr].individual, subtree.pop('individual'))
-                import ipdb; ipdb.set_trace()
-                tree[lyr].population = self.fasten(tree[lyr].population, subtree.pop('population'))
+                tree[lyr].individual = self.fasten(tree[lyr].individual,
+                                                   subtree.pop('individual'))
+                tree[lyr].population = self.fasten(tree[lyr].population,
+                                                   subtree.pop('population'))
             else:
-                tree[lyr] = self.fasten(tree[lyr], subtree)
+                tree[lyr].individual = self.fasten(tree[lyr].individual, subtree)
+                tree[lyr].population = self.fasten(tree[lyr].population, subtree)
             below = lyr
         subtree = tree.pop(below, None)
         tree = tree.level_of_analyses
@@ -47,22 +47,54 @@ class TreeMaker(object):
         for node in tree:
             if node != 'headline':
                 tree[node].next = subtree[grouping[node]]
+        # Set up computational sublayer
+        computational_sublayer_path = 'json' + os.sep + 'computational_sublayer' + '.json'
+        with open(computational_sublayer_path) as datafile:
+            computational_sublayer = json.load(datafile, object_pairs_hook=OrderedDict)
+        # Individual
+        for k in tree['1'].next.keys():
+            if k not in ('headline', 'next'):
+                for j in computational_sublayer['individual'].keys():
+                    temp = tree['1'].next[k].next[j].next
+                    tree['1'].next[k].next[j].next = computational_sublayer['individual'][j]
+                    tree['1'].next[k].next[j].next.next = temp
+        # Population
+        for i in ('2', '3', '4'):
+            for k in tree[i].next.keys():
+                if k not in ('headline', 'next'):
+                    for j in computational_sublayer['population'].keys():
+                        temp = tree[i].next[k].next[j].next
+                        tree[i].next[k].next[j].next = computational_sublayer['population'][j]
+                        tree[i].next[k].next[j].next.next = temp
+        # Set up statistics sublayer
+        statistics_sublayer_path = 'json' + os.sep + 'statistics_sublayer' + '.json'
+        with open(statistics_sublayer_path) as datafile:
+            statistics_sublayer = json.load(datafile, object_pairs_hook=OrderedDict)
+        # Individual
+        temp = tree['1'].next['N'].next
+        tree['1'].next['N'].next = statistics_sublayer['individual']['N']
+        tree['1'].next['N'].next.next = temp
+        # Population
+        for i in ('2', '3', '4'):
+            for j in statistics_sublayer['population'].keys():
+                temp = tree['2'].next[j].next
+                tree['2'].next[j].next = statistics_sublayer['population'][j]
+                tree['2'].next[j].next.next = temp
         return tree
 
 
 def spagedi_tree():
     # Import layers of the Spagedi decision tree from JSON files
-    layers = ['level_of_analyses', 'statistics', 'statistics_sublayer',
-              'computational_options', 'computational_sublayer',
-              'output_options']
+    layers = ['level_of_analyses', 'statistics', 'computational_options', 'output_options']
     tree = OrderedDict()
     for lyr in layers:
         json_path = 'json' + os.sep + lyr + '.json'
         with open(json_path) as datafile:
             tree[lyr] = json.load(datafile, object_pairs_hook=OrderedDict)
     tree = bunchify(tree)
-    tm = TreeMaker(tree.output_options)
-    return tm.buildtree(tree, layers[::-1])
+    tm = TreeMaker()
+    tree = tm.buildtree(tree, layers[::-1])
+    return tree
 
 def prp(o):
     print json.dumps(o, indent=3, sort_keys=True)
