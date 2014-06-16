df19fb25fa39b05eb538833a343928da7acf744c
Shaun Walbridge <shaun.walbridge@gmail.com>
2014-06-16 11:46:55 -0400

    reorganize paths to be heirarchical

--- Install/toolbox/scripts/add_install_path.py
+++ Install/toolbox/scripts/add_install_path.py
@@ -5,10 +5,10 @@ import os
 def add_install_path():
     local_path = os.path.dirname(__file__)
     add_paths = [
-            local_path, # scripts/
-            os.path.join(local_path, '..',), # toolbox/
-            os.path.join(local_path, '..', '..'), # Install/
-            os.path.join(local_path, '..', 'lib') # Install/lib/
+            os.path.join(local_path, '..', '..'), # Install
+            os.path.join(local_path, '..',), # Install/toolbox
+            os.path.join(local_path, '..', 'lib'), # Install/toolbox/lib
+            local_path # Install/toolbox/scripts
     ]
     for path in add_paths:
         full_path = os.path.abspath(path)
