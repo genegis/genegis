f7feb775237a826a0e83ac689055a35ae34ebf2c
Shaun Walbridge <shaun.walbridge@gmail.com>
2014-06-17 16:59:10 -0400

    A test FGDB with the full data table.

--- tests/consts.py
+++ tests/consts.py
@@ -4,10 +4,14 @@ import os
 local_path = os.path.dirname(__file__)
 data_path = os.path.join(local_path, 'data')
 
-# testing-specific constants.
+# testing-specific datasets.
 test_csv_doc = os.path.join(data_path, 'SRGD_example.csv')
 test_csv_with_comment_field = os.path.join(data_path, 'SRGD_with_comment_field.csv')
 test_csv_full = os.path.join(data_path, 'SRGD_export_demodna.csv.gz')
+
+# A File Geodatabasee with an imported SRGD file.
+test_fgdb = os.path.join(data_path, 'test.gdb')
+test_fgdb_data = os.path.join(test_fgdb, 'SRGD_export_demodna_Spatial')
 
 # our default set of columns.
 genetic_columns = "Sex;Haplotype;L_GATA417_1;L_GATA417_2;L_Ev37_1" + \
