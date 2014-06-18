f8a96da2c4705df498c10f7aef09c90b324a23e4
Shaun Walbridge <shaun.walbridge@gmail.com>
2014-06-18 14:29:41 -0400

    add test raster for sampling.

--- tests/consts.py
+++ tests/consts.py
@@ -9,9 +9,10 @@ test_csv_doc = os.path.join(data_path, 'SRGD_example.csv')
 test_csv_with_comment_field = os.path.join(data_path, 'SRGD_with_comment_field.csv')
 test_csv_full = os.path.join(data_path, 'SRGD_export_demodna.csv.gz')
 
-# A File Geodatabasee with an imported SRGD file.
+# A File Geodatabasee with an imported SRGD file and test raster.
 test_fgdb = os.path.join(data_path, 'test.gdb')
-test_fgdb_data = os.path.join(test_fgdb, 'SRGD_export_demodna_Spatial')
+test_fgdb_fc = os.path.join(test_fgdb, 'SRGD_export_demodna_Spatial')
+test_fgdb_raster = os.path.join(test_fgdb, 'etopo1_downsampled_clipped')
 
 # our default set of columns.
 genetic_columns = "Sex;Haplotype;L_GATA417_1;L_GATA417_2;L_Ev37_1" + \
