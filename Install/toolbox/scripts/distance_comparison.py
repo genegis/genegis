bc29b3b702040d88518bf487bb2f7a9a0f8d9177
Shaun Walbridge <shaun.walbridge@gmail.com>
2014-06-19 01:35:55 -0400

    Compare distance results between the GP tool and the ArcObjects version, against the debug DLL. Part of issue #90.

---
+++ Install/toolbox/scripts/distance_comparison.py
@@ -0,0 +1,69 @@
+# distance_comparison.py: a testing companion script to DistanceMatrix.py which 
+# compares between the high-speed geodesic form and the ArcPy based version.
+import arcpy
+import ctypes
+import os
+import sys
+
+import utils
+
+from DistanceMatrix import run_geodesic_gp
+
+def load_geodesic_dll():
+    fn = None
+    # load the DLL path from the config settings.
+    dll_path = os.path.abspath( \
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), \
+        '..', 'arcobjects', 'geodesic', 'Debug', 'geodesic.dll'))
+
+    if os.path.exists(dll_path):
+        try:
+            loaded_dll = ctypes.cdll.LoadLibrary(dll_path)
+        except Exception as e:
+            msg = "Failed to load high-speed geodesic library."
+            utils.msg(msg, mtype='error', exception=e)
+            return None
+        fn = loaded_dll.CalculatePairwiseGeodesicDistances
+        fn.argtypes = [ctypes.c_wchar_p, ctypes.c_wchar_p]
+        fn.restype = ctypes.c_int
+    return fn
+
+#input_fc = sys.argv[1]
+#output_matrix = sys.argv[2]
+input_fc = 'z:/data/arcgis/addins/genegis/tests/data/test.gdb/SRGD_tiny_Spatial'
+output_matrix = 'z:/data/arcgis/addins/genegis/tests/data/sample_dist.csv'
+output_matrix_gp = 'z:/data/arcgis/addins/genegis/tests/data/sample_dist_gp.csv'
+
+geodesic_cpp_fn = load_geodesic_dll()
+if geodesic_cpp_fn is not None:
+    desc = arcpy.Describe(input_fc)
+    # To run this, we need the full path to the input, not just the
+    # short one handed to us.
+    input_fc_fullpath = os.path.join(desc.path, desc.file)
+
+    print input_fc_fullpath
+
+    utils.msg("Loaded high-performance geodesic calculations, running...")
+    returncode = geodesic_cpp_fn(input_fc_fullpath, output_matrix)
+    if returncode == -1:
+        utils.msg("Cannot open the output file.", mtype='error')
+    elif returncode == -2:
+        utils.msg("Cannot open the input file.", mtype='error')
+    elif returncode == -3:
+        utils.msg("The input matrix is too large! consider" + \
+                "subsetting your results.", mtype='error')
+    elif returncode == -3:
+        utils.msg("This tool requires point features as input.", \
+                mtype='error')
+    elif returncode == 0:
+        utils.msg("Distance matrix calculations complete.")
+    else:
+        utils.msg("Unknown failure occured in high-performance " + \
+                "geodesic module.", mtype='error')
+
+    if returncode != 0:
+        sys.exit()
+
+# run the gp version for comparison
+utils.msg("Running the GP version...")
+run_geodesic_gp(input_fc, 1, output_matrix_gp, 3, False)
