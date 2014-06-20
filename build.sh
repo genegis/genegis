2d5070db8e58ac32f0127ef7c3f415ea2dce315c
Shaun Walbridge <shaun.walbridge@gmail.com>
2014-06-20 00:36:05 -0400

    Improve robustness of build process.

--- build.sh
+++ build.sh
@@ -3,5 +3,19 @@ echo "building Addin..."
 python ./makeaddin.py
 echo "signing Addin..."
 # "/c/Program Files (x86)/Common Files/ArcGIS/bin/ESRISignAddin.exe" c:\\data\\arcgis\\addins\\genegis\\genegis.esriaddin /c/ssh/id_rsa.ppk
-# start /c/Users/shau7031/Documents/ArcGIS/genegis.mxd
-start ~/Documents/ArcGIS/genegis.mxd
+echo "Would you like to install the add-in? y/N"
+read input
+if [ "$input" == "y" ]; then
+  start genegis.esriaddin
+else
+  echo 'skipping installation.'
+fi
+
+echo "Would you like to open the geneGIS MXD? y/N"
+read input
+if [ "$input" == "y" ]; then
+  # start /c/Users/shau7031/Documents/ArcGIS/genegis.mxd
+  start \\\\psf\\Home\\Documents\\ArcGIS\\genegis.mxd
+else
+  echo 'not opening MXD.'
+fi
