7931abe2b4e4e7e476b39019027ae83b041d4567
Shaun Walbridge <shaun.walbridge@gmail.com>
2014-07-06 10:59:39 -0400

    Add error message for failure to find geodesic DLL case.

--- Install/toolbox/scripts/DistanceMatrix.py
+++ Install/toolbox/scripts/DistanceMatrix.py
@@ -90,6 +90,8 @@ def load_geodesic_dll():
         fn = loaded_dll.CalculatePairwiseGeodesicDistances
         fn.argtypes = [ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_double, ctypes.c_bool]
         fn.restype = ctypes.c_int
+    else:
+        utils.msg("Unable to locate high-speed geodesic DLL at: {}".format(dll_path))
     return fn                
 
 def run_geodesic_gp(input_fc, unit_factor, output_matrix, row_count, is_spagedi):
